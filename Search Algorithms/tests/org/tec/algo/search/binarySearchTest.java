1b0d726bd5ec8d2c88ba9f8c8a90a368da13bdfe
sebas97012 <sebas.smr@hotmail.com>
2017-06-04 11:30:24 -0600

    All tests started2

---
+++ Search Algorithms/tests/org/tec/algo/search/binarySearchTest.java
@@ -0,0 +1,15 @@
+package org.tec.algo.search;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sebas97012 on 6/4/17.
+ */
+public class binarySearchTest {
+    @Test
+    public void binarySearch1() throws Exception {
+    }
+
+}
