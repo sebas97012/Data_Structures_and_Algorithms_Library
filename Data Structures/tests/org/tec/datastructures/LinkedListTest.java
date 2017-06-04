d0e741d586afd2f693c5e58dd14082dcf2698070
sebas97012 <sebas.smr@hotmail.com>
2017-06-04 10:55:26 -0600

    Tests initialized 2

--- Data Structures/tests/org/tec/datastructures/LinkedListTest.java
+++ Data Structures/tests/org/tec/datastructures/LinkedListTest.java
@@ -5,8 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 
-import static org.junit.Assert.*;
-
 /**
  * Created by sebas97012 on 6/3/17.
  */
@@ -18,59 +16,67 @@ public class LinkedListTest {
         for(int i=0; i<=1000; i++){
             TestList.insertElement((int) (Math.random() * ((1000) + 1)), i);
         }
-        TestList.printList();
     }
     @Test
     public void getFirst() throws Exception {
-
+        System.out.println(TestList.getFirst().getDataT());
     }
 
     @Test
     public void setFirst() throws Exception {
+        TestList.setFirst(new Node<String>("hola"));
+        System.out.println(TestList.getFirst().getDataT());
     }
 
     @Test
     public void getSize() throws Exception {
+        System.out.println(TestList.getSize());
     }
 
     @Test
     public void insertAtFirst() throws Exception {
+        TestList.insertAtFirst(233);
+        System.out.println(TestList.getFirst().getDataT());
     }
 
     @Test
     public void insertAtEnd() throws Exception {
+        TestList.insertAtEnd("Final");
     }
 
     @Test
     public void getElement() throws Exception {
+        System.out.println(TestList.getElement((int) (Math.random() * ((1000) + 1))).getDataT());
     }
 
     @Test
     public void deleteFirst() throws Exception {
+        TestList.deleteFirst();
     }
 
     @Test
     public void insertElement() throws Exception {
+        TestList.insertElement(2333,(int) (Math.random() * ((1000) + 1)));
     }
 
     @Test
     public void deleteElement() throws Exception {
+        TestList.deleteElement(143);
     }
 
     @Test
     public void deleteElement1() throws Exception {
+        TestList.deleteElementPos((int) (Math.random() * ((1000) + 1)));
     }
 
     @Test
     public void cleanList() throws Exception {
-    }
-
-    @Test
-    public void printList() throws Exception {
+    TestList.cleanList();
     }
 
    @After
     public void after() throws Exception {
+       TestList.printList();
         TestList.cleanList();
     }
 }
